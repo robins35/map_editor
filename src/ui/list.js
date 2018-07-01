2b203d6c6e1b75fd0881d664e4cc78bb6d04cf9e
Caiden Robinson <caiden.robinson35@gmail.com>
2018-07-01 01:53:48 -0700

    User can scroll a list with arrow keys

--- src/ui/list.js
+++ src/ui/list.js
@@ -13,6 +13,8 @@ export default class List extends UIElement {
     properties["borderWidth"] = 1
     super(canvas, properties, skipChildCreation)
     this.name = "UI.List"
+    this.currentStartingIndex = 0
+    this.currentViewSlice = this.children.slice(0, this.maxItems)
 
     // if(this.items != undefined && this.items.length > 0) {
     //   super.createChildElements(this)
@@ -68,6 +70,8 @@ export default class List extends UIElement {
     this.convertItemsToUIFormat()
     if(this.items != undefined && this.items.length > 0) {
       super.createChildElements({ children: this.children })
+      this.currentStartingIndex = 0
+      this.currentViewSlice = this.children.slice(0, this.maxItems)
     }
   }
 
@@ -77,5 +81,72 @@ export default class List extends UIElement {
       target = target.parent
     }
     target.selectedItem = this
+  }
+
+  update() {
+    this.handleArrowkeyScrolling()
+    super.update()
+  }
+
+  setSelectedItem(item) {
+    this.selectedItem = item
+    Text.selectedText = item.id
+  }
+
+  handleArrowkeyScrolling() {
+    if(this.event_object.keysDown[38]) {
+      if (this.selectedItem && this.selectedItem.previousSibling) {
+        if(this.selectedItem.id == this.currentViewSlice[0].id)
+          this.shiftListView(-1)
+
+        this.setSelectedItem(this.selectedItem.previousSibling)
+      }
+      else {
+        this.setSelectedItem(this.children[0])
+      }
+
+      delete this.event_object.keysDown[38]
+    }
+
+    if(this.event_object.keysDown[40]) {
+      if(this.selectedItem) {
+        if (this.selectedItem.nextSibling) {
+          if(this.selectedItem.id == this.currentViewSlice.slice(-1)[0].id)
+            this.shiftListView(1)
+
+          this.setSelectedItem(this.selectedItem.nextSibling)
+        }
+      }
+      else {
+        this.setSelectedItem(this.children[0])
+      }
+
+      delete this.event_object.keysDown[40]
+    }
+  }
+
+  shiftListView(i) {
+    let newStartingIndex = this.currentStartingIndex + i
+    let newEndingBound = newStartingIndex + this.maxItems
+
+    if(newStartingIndex < 0 || newEndingBound > this.children.length)
+      return
+
+    let newSlice = this.children.slice(newStartingIndex, newEndingBound)
+
+    // Resetting visibility the lazy way here. I could only turn off visibility
+    // for the effected items, but the list will never be very large so it
+    // shouldn't have an effect on performance.
+    for(let item of this.currentViewSlice) {
+      item.visible = false
+    }
+
+    for(let [index, item] of newSlice.entries()) {
+      item.pos.y = this.pos.y + (index * this.rowHeight)
+      item.visible = true
+    }
+
+    this.currentStartingIndex = newStartingIndex
+    this.currentViewSlice = newSlice
   }
 }
